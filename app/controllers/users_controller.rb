3630fc9de6c15452d07d6a8ef72b89b50c425fbe
kadrianne <kristine.a.du@gmail.com>
2020-04-03 14:50:12 -0600

    Add success message.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
-            render json: @user
+            render json: {message: "User successfully created!", user: @user}
         else
             render json: @user.errors.messages
         end
