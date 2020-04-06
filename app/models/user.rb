e87c9a1644b4180d86b2760443423f6905fbf502
kadrianne <kristine.a.du@gmail.com>
2020-04-05 21:31:18 -0600

    Update validations and clear database.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-    has_secure_password
+    has_secure_password validations: false
 
-    validates :name, :username, :email, presence: {message: "Please enter a %{attribute}."}
+    validates :name, :username, :email, :password, presence: {message: "%{attribute} cannot be blank."}
     validates :username, :password, length: {in: 6..20, message: "%{attribute} must be between 6 and 20 characters long."}
     validates :username, uniqueness: {message: "%{attribute}s must be unique, and %{value} has already been taken."}
 
